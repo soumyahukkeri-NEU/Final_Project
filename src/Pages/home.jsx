ddfab9a1479406524bfd2e4a3d5233a5bf83eaab
Mingyue <145176349+LiuMingyue1@users.noreply.github.com>
2023-11-29 01:53:03 -0500

    finish index-image component

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Text from "../components/text";
 import AllLink from "../components/link";
+import Indeximage from "../components/indeximage";
 
 const Home = () => {
     return (
@@ -28,30 +29,24 @@ const Home = () => {
 
               <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                   <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
-                    <div class="relative">
-                      <img className="northern lights.jpg" src="public/assets/northern lights.jpg" alt="Picture of Northern Lights" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
-                      <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                    </div>
+                    <Indeximage className="northern lights.jpg" src="public/assets/northern lights.jpg" alt="Picture of Northern Lights">
+                    </Indeximage>
                   </div>
+
                   <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
-                    <div class="relative">
-                      <img className="paris.jpg" src="public/assets/paris.jpg" alt="Picture of Paris" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
-                      <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                    </div>
-                    <div class="relative">
-                      <img className="switzerland.jpg" src="public/assets/switzerland.jpg" alt="Picture of Switzerland" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
-                      <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                    </div>
+                    <Indeximage className="paris.jpg" src="public/assets/paris.jpg" alt="Picture of Paris">
+                    </Indeximage>
+                  
+                    <Indeximage className="switzerland.jpg" src="public/assets/switzerland.jpg" alt="Picture of Switzerland">
+                    </Indeximage>
                   </div>
+
                   <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
-                    <div class="relative">
-                      <img className="maldives.jpg" src="public/assets/maldives.jpg" alt="Picture of Maldives" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
-                      <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                    </div>
-                    <div class="relative">
-                      <img className="prague.jpg" src="public/assets/prague.jpg" alt="Picture of Prague" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
-                      <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-                    </div>
+                    <Indeximage className="maldives.jpg" src="public/assets/maldives.jpg" alt="Picture of Maldives">
+                    </Indeximage>
+                  
+                    <Indeximage className="prague.jpg" src="public/assets/prague.jpg" alt="Picture of Prague">
+                    </Indeximage>
                   </div>
               </div>
               
--- src/components/indeximage.jsx
+++ src/components/indeximage.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
 
+const Indeximage = ({ className, src, alt }) => {
+    return (
+           <div class="relative">
+                <img className={className} src={src} alt={alt} class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"/>
+                <div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
+            </div>
+    );
+}
+
+export default Indeximage;
