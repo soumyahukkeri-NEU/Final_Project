d6a7c9e2b372904271bea6ffb8c3e30ca8c84539
Leqing Wu <leqingwu@LeqingdeMacBook-Air.local>
2023-12-05 17:15:56 -0500

    1

--- src/Pages/herosection.jsx
+++ src/Pages/herosection.jsx
@@ -7,15 +7,10 @@ const HeroSection = () => {
                 <main>
                     <div className="relative isolate">
                         <div className="overflow-hidden">
-                            <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
-                                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+                            <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-4 lg:pt-32"> {/* Decreased lg:px-8 to lg:px-4 */}
+                                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center justify-center"> {/* Added justify-center */}
                                     <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-                                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Voyage</h1>
-                                        <h5 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your Journey, Our Passion</h5>
-                                        <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">We believe in the power of exploration, the thrill of adventure, and the magic of discovering new horizons. Join us as we embark on a voyage like no other, where every step is a story, and every moment is an unforgettable memory. Let's make your journey extraordinary, together!</p>
-                                        <div className="mt-10 flex items-center gap-x-6">
-                                            <a href="#jump" className="text-sm font-semibold leading-6 text-gray-900">Get Started <span aria-hidden="true">→</span></a>
-                                        </div>
+                                        {/* Your existing content */}
                                     </div>
                                     <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                                         <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
