030fe38287802d10f983cbbcc68f4b17ac3b917c
Mingyue <145176349+LiuMingyue1@users.noreply.github.com>
2023-12-06 23:21:54 -0500

    contact info

--- src/Pages/contactus.jsx
+++ src/Pages/contactus.jsx
@@ -6,6 +6,16 @@ const ContactUs = () => {
     return (
       <div>
         <ContactForm></ContactForm>
+        <br></br>
+          <div class="relative">
+            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+              <div class="w-full border-t border-gray-300"></div>
+            </div>
+            <div class="relative flex justify-center">
+              <span class="bg-white px-3 text-lg font-semibold leading-6 text-gray-900">Didnt find what you're looking for?</span>
+            </div>
+          </div>
+        <br></br>
         <ContactUsInfo></ContactUsInfo>
         <Footer></Footer>
       </div>
