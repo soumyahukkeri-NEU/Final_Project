ab5a4db5ba4fc8e183368df1f0c6d21b8b3e33f8
Soumya Hukkeri <hukkeri.so@northeastern.edu>
2023-12-07 18:22:18 -0500

    modifications

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -10,48 +10,45 @@ import TestimonialContent from "../components/TestimonialContent";
 const Home = () => {
   return (
     <>
-    <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:pt-32">
-      <div className="max-w-2xl mx-auto gap-x-14 lg:flex lg:max-w-none lg:items-center">
-
-        <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-          <Text
-            title1="Voyage"
-            title2="Your Journey, Our Passion"
-            text="We believe in the power of exploration, the thrill of adventure, and the magic of discovering new horizons. Join us as we embark on a voyage like no other, where every step is a story, and every moment is an unforgettable memory. Let's make your journey extraordinary, together!" />
-
-               <div className="mt-10 flex items-center gap-x-6">
-        
-                  <AllLink 
-                      to="/destination" 
-                      className="text-sm font-semibold leading-6 text-gray-900"
-                  >
-                      Get Started <span aria-hidden="true">→</span>
-                </AllLink>
+    <div className="relative isolate">
+      <div className="overflow-hidden">
+        <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+          <div className="mx-auto max-w-2xl gap-x-14 lg:flex lg:max-w-none lg:items-center">
+            <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Voyage</h1>
+              <br />
+              <h5 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your Journey, Our Passion</h5>
+              <p className="relative mt-4 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+                We believe in the power of exploration, the thrill of adventure, and the magic of discovering new horizons. Join us as we embark on a voyage like no other, where every step is a story, and every moment is an unforgettable memory. Let's make your journey extraordinary, together!
+              </p>
+              <div className="mt-4 flex items-center gap-x-6"> {/* Adjusted the margin-top here */}
+                <AllLink to="/destination" className="text-sm font-semibold leading-6 text-gray-900">Get Started <span aria-hidden="true">→</span></AllLink>
               </div>
-
-              <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
-                  <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
-                    <Indeximage className="northern lights.jpg" src="public/assets/northern lights.jpg" alt="Picture of Northern Lights">
-                    </Indeximage>
-                  </div>
-
-                  <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
-                    <Indeximage className="paris.jpg" src="public/assets/paris.jpg" alt="Picture of Paris">
-                    </Indeximage>
-                  
-                    <Indeximage className="switzerland.jpg" src="public/assets/switzerland.jpg" alt="Picture of Switzerland">
-                    </Indeximage>
-                  </div>
-
-                  <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
-                    <Indeximage className="maldives.jpg" src="public/assets/maldives.jpg" alt="Picture of Maldives">
-                    </Indeximage>
-                  
-                    <Indeximage className="prague.jpg" src="public/assets/prague.jpg" alt="Picture of Prague">
-                    </Indeximage>
-                  </div>
+            </div>
+            <div className="mt-4 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-0 lg:mt-0 lg:pl-0"> {/* Adjusted the margin-top here */}
+              <div className="ml-auto w-36 flex-none space-y-8 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+                <div className="relative">
+                  <Indeximage className="northernlights.jpg" src="public/assets/northernlights.jpg" alt="Picture of Northern Lights" />
+                </div>
               </div>
-              
+              <div className="mr-auto w-36 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+                <div className="relative">
+                  <Indeximage className="paris.jpg" src="public/assets/paris.jpg" alt="Picture of Paris" />
+                </div>
+                <div className="relative">
+                  <Indeximage className="switzerland.jpg" src="public/assets/switzerland.jpg" alt="Picture of Switzerland" />
+                </div>
+              </div>
+              <div className="w-36 flex-none space-y-8 pt-32 sm:pt-0">
+                <div className="relative">
+                  <Indeximage className="maldives.jpg" src="public/assets/maldives.jpg" alt="Picture of Maldives" />
+                </div>
+                <div className="relative">
+                  <Indeximage className="prague.jpg" src="public/assets/prague.jpg" alt="Picture of Prague" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
